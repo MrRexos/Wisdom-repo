fadf673f171e0ef48757fb0d1c699ff16d7f9708
MrRexos <123406399+MrRexos@users.noreply.github.com>
2024-08-11 00:38:18 +0200

    Trying to connect via laptop

--- Wisdom_expo/screens/WelcomeVideoScreen.js
+++ Wisdom_expo/screens/WelcomeVideoScreen.js
@@ -4,12 +4,17 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Navigation from '../navigation/navigation';
 
+import { useNavigation } from '@react-navigation/native';
+
+
 export default function WelcomeVideoScreen() {
     const {colorScheme, toggleColorScheme} = useColorScheme();
+    const navigation = useNavigation();
     return (
         <View className='flex-1 justify-center items-center bg-[#f2f2f2] dark:bg-[#272626]'>
           <Text className='text-[#272626] dark:text-[#f2f2f2]'>WelcomeVideoScreen</Text>
           <Switch value={colorScheme==='dark'} onChange={toggleColorScheme}/>
+          <Button title='Hola' onPress={() => navigation.navigate('Loading')} />
       </View>
     )
 }
