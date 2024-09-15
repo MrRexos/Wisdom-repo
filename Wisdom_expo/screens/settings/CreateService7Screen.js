c83b31bfc5e161d5962cce57715a8be56c31cd68
MrRexos <hernanz.reio@gmail.com>
2024-09-15 23:00:56 +0200

    Doing create service 7

--- Wisdom_expo/screens/settings/CreateService7Screen.js
+++ Wisdom_expo/screens/settings/CreateService7Screen.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect  } from 'react';
-import { View, StatusBar, SafeAreaView, Platform, TouchableOpacity, Text, TextInput, ScrollView, Keyboard } from 'react-native';
+import { View, StatusBar, SafeAreaView, Platform, TouchableOpacity, Text, TextInput, ScrollView, Keyboard, StyleSheet} from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useColorScheme } from 'nativewind';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { XMarkIcon } from 'react-native-heroicons/outline';
-import DateTimePickerModal from "react-native-modal-datetime-picker";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 export default function CreateService7Screen() {
@@ -18,13 +18,14 @@ export default function CreateService7Screen() {
   const {title, family, category, description, selectedLanguages, isIndividual, hobbies, tags, location, actionRate} = route.params;
   const [position, setPosition] = useState('');
   const [place, setPlace] = useState('');
-  const [experiences, setExperiences] = useState();
+  const [experiences, setExperiences] = useState([]);
   const [experienceItem, setExperienceItem] = useState([]);
   const [showAddExperience, setShowAddExperience] = useState(false);
-  const [startDate, setStartDate] = useState(new Date())
-  const [endDate, setEndDate] = useState(new Date())
-  const [openStartDate, setOpenStartDate] = useState(false)
-  const [openEndDate, setOpenEndDate] = useState(false)
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState();
+  const [openStartDate, setOpenStartDate] = useState(false);
+  const [openEndDate, setOpenEndDate] = useState(false);
+  const [tempDate, setTempDate] = useState(new Date());
 
   const inputPositionChanged = (text) => {
     setPosition(text);
@@ -34,10 +35,73 @@ export default function CreateService7Screen() {
     setPlace(text);
   };
 
-  const handleConfirm = (date) => {
-    console.log("A date has been picked: ", date);
-    setOpenStartDate(false);;
+  const handleStartDateChange = (event, date) => {
+    if (date !== undefined) {
+      setTempDate(date); // Almacena la fecha seleccionada temporalmente
+    }
   };
+
+  const handleAcceptStartDate = () => {
+    setStartDate(tempDate); 
+    setTempDate(new Date());
+    setOpenStartDate(false);  
+  };
+
+  const handleCancelStartDate = () => {
+    setTempDate(new Date());
+    setOpenStartDate(false); 
+    
+  };
+
+
+  const handleEndDateChange = (event, date) => {
+    if (date !== undefined) {
+      setTempDate(date); 
+    }
+  };
+
+  const handleAcceptEndDate = () => {
+    const today = new Date();
+    
+    const isToday = (
+      tempDate.getDate() === today.getDate() &&
+      tempDate.getMonth() === today.getMonth() &&
+      tempDate.getFullYear() === today.getFullYear()
+    );
+
+    if (tempDate >= today || isToday) {
+      setEndDate(null);
+    } else {
+      setEndDate(tempDate); // Si es en el pasado, establecer la fecha seleccionada
+    }
+  
+    setTempDate(new Date()); // Restablecer la fecha temporal a la fecha actual
+    setOpenEndDate(false); // Cerrar el DateTimePicker
+  };
+
+  const handleCancelEndDate = () => {
+    setTempDate(new Date());
+    setOpenEndDate(false); 
+  };
+
+  const handleSave = () => {
+    experiences.push({position: position, place: place, startDate: startDate, endDate: endDate})
+    setPosition('');
+    setPlace('');
+    setStartDate(new Date());
+    setEndDate();
+    setShowAddExperience(false);
+  };
+
+  const handleCloseAddExperience = () => {
+    setPosition('');
+    setPlace('');
+    setStartDate(new Date());
+    setEndDate();
+    setShowAddExperience(false);
+  };
+
+
 
   return (
     <SafeAreaView style={{ flex: 1, paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }} className='flex-1 bg-[#f2f2f2] dark:bg-[#272626]'>
@@ -57,9 +121,23 @@ export default function CreateService7Screen() {
               <Text className="mt-5 font-inter-bold text-[15px] text-center text-[#b6b5b5] dark:text-[#706f6e]">Show your entire career with this service</Text>
             </View>
 
-            {experiences? (
+            {experiences.length>0? (
               <View className="flex-1 justify-center items-center">
 
+                {experiences.map((exp, index) => (
+                  <View key={index} className="w-full py-3 px-5 bg-[#fcfcfc] dark:bg-[#323131] rounded-2xl mb-3">
+                    <Text className="font-inter-semibold text-[17px] text-[#444343] dark:text-[#f2f2f2]">{exp.position}</Text>
+                    <View className="mt-3 flex-row justify-between items-center">
+                      <Text className="font-inter-medium text-[12px] text-[#706F6E] dark:text-[#b6b5b5]">{exp.place}</Text>
+                      <Text>
+                        <Text className=" text-[12px] text-[#706F6E] dark:text-[#b6b5b5]">{exp.startDate.toLocaleDateString()} </Text>
+                        <Text className=" text-[12px] text-[#706F6E] dark:text-[#b6b5b5]"> - </Text>
+                        <Text className=" text-[12px] text-[#706F6E] dark:text-[#b6b5b5]">{exp.endDate ? exp.endDate.toLocaleDateString() : 'Still here'}</Text>
+                      </Text>
+                    </View>
+                  </View>
+                ))}
+                
               </View>
             ) : null }
 
@@ -73,9 +151,9 @@ export default function CreateService7Screen() {
 
             ): (
               <View className="flex-1 justify-center items-center">
-                <View className="w-full h-[330] mt-7 py-4 px-5 bg-[#fcfcfc] dark:bg-[#323131] rounded-2xl">
+                <View className="w-full mt-7 py-4 px-5 bg-[#fcfcfc] dark:bg-[#323131] rounded-2xl">
 
-                  <TouchableOpacity onPress={() => setShowAddExperience(false)} className="justify-center items-end">
+                  <TouchableOpacity onPress={() => handleCloseAddExperience()} className="justify-center items-end">
                     <XMarkIcon size={23} color={iconColor} strokeWidth="2" />
                   </TouchableOpacity>
 
@@ -113,25 +191,75 @@ export default function CreateService7Screen() {
 
                     <TouchableOpacity onPress={() => setOpenStartDate(true)} className="flex-1 justify-center items-center">
                       <Text className="font-inter-semibold text-[14px] text-center underline underline-offset-2 text-[#444343] dark:text-[#f2f2f2]">Started</Text>
-                      <Text className="mt-3 font-inter-medium text-[14px] text-center  text-[#b6b5b5] dark:text-[#706f6e]">July</Text>
+                      <Text className="mt-3 font-inter-medium text-[14px] text-center  text-[#b6b5b5] dark:text-[#706f6e]">{startDate.toLocaleDateString()}</Text>
                     </TouchableOpacity>
-                    <DateTimePickerModal
-                      isVisible={openStartDate}
-                      mode="datetime"
-                      onConfirm={handleConfirm}
-                      onCancel={() => setOpenStartDate(false)}
-                    />
 
-                    <TouchableOpacity className="flex-1 justify-center items-center">
-                      <Text className="font-inter-semibold text-[14px] text-center underline underline-offset-2 text-[#444343] dark:text-[#f2f2f2]">End</Text>
-                      <Text className="mt-3 font-inter-medium text-[14px] text-center  text-[#b6b5b5] dark:text-[#706f6e]">Still here</Text>
+                    {openStartDate && (
+                      <View>
+                        <View className="w-[300] h-[110]">
+                          <DateTimePicker
+                            value={tempDate}
+                            mode="date"
+                            display="spinner"
+                            onChange={handleStartDateChange}
+                            style={styles.picker}
+                          />
+                        </View>
+                        {Platform.OS === 'ios' ? (
+                          <View className="flex-row justify-between px-12">
+                            <TouchableOpacity onPress={handleCancelStartDate}>
+                              <Text className="mt-3 font-inter-medium text-[15px] text-center text-[#b6b5b5] dark:text-[#706f6e]">Cancel</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={handleAcceptStartDate}>
+                              <Text className="mt-3 font-inter-semibold text-[15px] text-center text-[#706F6E] dark:text-[#b6b5b5]">Accept</Text>
+                            </TouchableOpacity>
+                          </View>
+                        ) : null}
+
+                      </View>
+                    )}
+
+                    <TouchableOpacity onPress={() => setOpenEndDate(true)} className="flex-1 justify-center items-center">
+                      <Text className={`font-inter-semibold text-[14px] text-center underline underline-offset-2 ${endDate && endDate < startDate ? 'text-[#ff633e]' : 'text-[#444343] dark:text-[#f2f2f2]'}`}>End</Text>
+                      <Text className="mt-3 font-inter-medium text-[14px] text-center  text-[#b6b5b5] dark:text-[#706f6e]">{endDate? endDate.toLocaleDateString(): 'Still here'}</Text>
                     </TouchableOpacity>
+
+                    {openEndDate && (
+                      <View>
+                        <View className="w-[300] h-[110]">
+                          <DateTimePicker
+                            value={tempDate}
+                            mode="date"
+                            display="spinner"
+                            onChange={handleEndDateChange}
+                            style={styles.picker}
+                          />
+                        </View>
+                        {Platform.OS === 'ios' ? (
+                          <View className="flex-row justify-between px-12">
+                            <TouchableOpacity onPress={handleCancelEndDate}>
+                              <Text className="mt-3 font-inter-medium text-[15px] text-center text-[#b6b5b5] dark:text-[#706f6e]">Cancel</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={handleAcceptEndDate}>
+                              <Text className="mt-3 font-inter-semibold text-[15px] text-center text-[#706F6E] dark:text-[#b6b5b5]">Accept</Text>
+                            </TouchableOpacity>
+                          </View>
+                        ) : null}
+
+                      </View>
+                    )}
                     
 
                   </View>
 
-                  <TouchableOpacity className="mt-3 justify-center items-center">
-                    <Text className="mt-5 font-inter-bold text-[15px] text-center text-[#706F6E] dark:text-[#b6b5b5]">Save</Text>
+                  <TouchableOpacity
+                    disabled={endDate && endDate < startDate || position.length === 0} 
+                    className={`mt-3 justify-center items-center`}
+                    onPress={() => handleSave()}
+                  >
+                    <Text className="mt-5 font-inter-bold text-[15px] text-center text-[#706F6E] dark:text-[#b6b5b5]">
+                      Save
+                    </Text>
                   </TouchableOpacity>
 
                 </View>
@@ -161,3 +289,10 @@ export default function CreateService7Screen() {
     </SafeAreaView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  picker: {
+    flex: 1,
+  },
+});
