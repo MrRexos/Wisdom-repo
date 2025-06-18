5f097170c134876ec32af38c7b990b456a661d50
Mr Rexos <136998090+MrRexos@users.noreply.github.com>
2025-06-18 23:15:23 +0200

    feat: add change password screen

---
+++ Wisdom_expo/screens/settings/ChangePasswordScreen.js
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from 'react';
+import { View, SafeAreaView, StatusBar, Platform, Text, TouchableOpacity, TextInput, Keyboard, Alert } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import '../../languages/i18n';
+import { useColorScheme } from 'nativewind';
+import { useNavigation } from '@react-navigation/native';
+import { ChevronLeftIcon } from 'react-native-heroicons/outline';
+import EyeIcon from 'react-native-bootstrap-icons/icons/eye';
+import EyeSlashIcon from 'react-native-bootstrap-icons/icons/eye-slash';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { getDataLocally } from '../../utils/asyncStorage';
+import api from '../../utils/api';
+
+export default function ChangePasswordScreen() {
+  const { colorScheme } = useColorScheme();
+  const { t } = useTranslation();
+  const navigation = useNavigation();
+  const [user, setUser] = useState(null);
+
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [isSecureCurrent, setIsSecureCurrent] = useState(true);
+  const [isSecurePassword, setIsSecurePassword] = useState(true);
+  const [isSecureConfirm, setIsSecureConfirm] = useState(true);
+
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [keyboardOpen, setKeyboardOpen] = useState(false);
+
+  const iconColor = colorScheme === 'dark' ? '#f2f2f2' : '#444343';
+  const placeHolderTextColorChange = colorScheme === 'dark' ? '#706F6E' : '#B6B5B5';
+  const cursorColorChange = colorScheme === 'dark' ? '#f2f2f2' : '#444343';
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const userData = await getDataLocally('user');
+      if (userData) {
+        setUser(JSON.parse(userData));
+      }
+    };
+    getUserData();
+  }, []);
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => setKeyboardOpen(true));
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => setKeyboardOpen(false));
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  const formValid = currentPassword.length > 0 && newPassword.length >= 8 && newPassword === confirmPassword;
+
+  const handleChangePassword = async () => {
+    if (!formValid || !user) return;
+    try {
+      await api.put(`/api/user/${user.id}/password`, {
+        currentPassword: currentPassword,
+        newPassword: newPassword,
+      });
+      navigation.goBack();
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.error) {
+        setErrorMessage(error.response.data.error);
+      } else {
+        setErrorMessage('Error');
+      }
+      setShowError(true);
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }} className='flex-1 bg-[#f2f2f2] dark:bg-[#272626]'>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <View className="absolute bg-[#f2f2f2] dark:bg-[#272626] h-[95] w-full z-10 justify-end">
+        <View className="flex-row justify-between items-center pb-4 px-2">
+          <View className="flex-1">
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <ChevronLeftIcon size={24} strokeWidth={1.7} color={iconColor} />
+            </TouchableOpacity>
+          </View>
+          <View className="flex-1 justify-center items-center">
+            <Text className="font-inter-semibold text-center text-[16px] text-[#444343] dark:text-[#f2f2f2]">{t('change_password')}</Text>
+          </View>
+          <View className="flex-1 justify-center items-end">
+            {formValid && (
+              <TouchableOpacity onPress={handleChangePassword} className="mr-2 justify-center items-center rounded-full px-3 py-2 bg-[#E0E0E0] dark:bg-[#3D3D3D]">
+                <Text className="font-inter-medium text-[13px] text-[#444343] dark:text-[#f2f2f2]">{t('done')}</Text>
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
+      </View>
+      <KeyboardAwareScrollView style={{ flex: 1, width: '100%' }} enableOnAndroid={true} scrollEnabled={keyboardOpen}>
+        <View className="px-5 pt-[75] w-full">
+          <Text className="font-inter-semibold text-[15px] pt-6 text-[#444343] dark:text-[#f2f2f2]">{t('current_password')}</Text>
+          <View className="mt-3 px-5 h-[55] flex-row justify-between items-center rounded-full bg-[#E0E0E0]/60 dark:bg-[#3D3D3D]/60 border-[1px] border-[#706F6E]/20 dark:border-[#B6B5B5]/20">
+            <TextInput
+              placeholder={t('current_password')}
+              secureTextEntry={isSecureCurrent}
+              selectionColor={cursorColorChange}
+              placeholderTextColor={placeHolderTextColorChange}
+              onChangeText={(text) => { setCurrentPassword(text); setShowError(false); }}
+              value={currentPassword}
+              keyboardAppearance={colorScheme === 'dark' ? 'dark' : 'light'}
+              className="text-[15px] h-[55] flex-1 text-[#444343] dark:text-[#f2f2f2]"
+            />
+            <TouchableOpacity onPress={() => setIsSecureCurrent(!isSecureCurrent)}>
+              {isSecureCurrent ? (
+                <EyeSlashIcon size={20} color={placeHolderTextColorChange} style={{ marginLeft: 10, transform: [{ scale: 1.15 }] }} />
+              ) : (
+                <EyeIcon size={20} color={placeHolderTextColorChange} style={{ marginLeft: 10, transform: [{ scale: 1.15 }] }} />
+              )}
+            </TouchableOpacity>
+          </View>
+          <Text className="font-inter-semibold text-[15px] pt-6 text-[#444343] dark:text-[#f2f2f2]">{t('new_password')}</Text>
+          <View className="mt-3 px-5 h-[55] flex-row justify-between items-center rounded-full bg-[#E0E0E0]/60 dark:bg-[#3D3D3D]/60 border-[1px] border-[#706F6E]/20 dark:border-[#B6B5B5]/20">
+            <TextInput
+              placeholder={t('new_password')}
+              secureTextEntry={isSecurePassword}
+              selectionColor={cursorColorChange}
+              placeholderTextColor={placeHolderTextColorChange}
+              onChangeText={(text) => { setNewPassword(text); setShowError(false); }}
+              value={newPassword}
+              keyboardAppearance={colorScheme === 'dark' ? 'dark' : 'light'}
+              className="text-[15px] h-[55] flex-1 text-[#444343] dark:text-[#f2f2f2]"
+            />
+            <TouchableOpacity onPress={() => setIsSecurePassword(!isSecurePassword)}>
+              {isSecurePassword ? (
+                <EyeSlashIcon size={20} color={placeHolderTextColorChange} style={{ marginLeft: 10, transform: [{ scale: 1.15 }] }} />
+              ) : (
+                <EyeIcon size={20} color={placeHolderTextColorChange} style={{ marginLeft: 10, transform: [{ scale: 1.15 }] }} />
+              )}
+            </TouchableOpacity>
+          </View>
+          <Text className="font-inter-semibold text-[15px] pt-6 text-[#444343] dark:text-[#f2f2f2]">{t('confirm_new_password')}</Text>
+          <View className="mt-3 px-5 h-[55] flex-row justify-between items-center rounded-full bg-[#E0E0E0]/60 dark:bg-[#3D3D3D]/60 border-[1px] border-[#706F6E]/20 dark:border-[#B6B5B5]/20">
+            <TextInput
+              placeholder={t('type_it_again')}
+              secureTextEntry={isSecureConfirm}
+              selectionColor={cursorColorChange}
+              placeholderTextColor={placeHolderTextColorChange}
+              onChangeText={(text) => { setConfirmPassword(text); setShowError(false); }}
+              value={confirmPassword}
+              keyboardAppearance={colorScheme === 'dark' ? 'dark' : 'light'}
+              className="h-[55] flex-1 text-[15px] text-[#444343] dark:text-[#f2f2f2]"
+            />
+            <TouchableOpacity onPress={() => setIsSecureConfirm(!isSecureConfirm)}>
+              {isSecureConfirm ? (
+                <EyeSlashIcon size={20} color={placeHolderTextColorChange} style={{ marginLeft: 10, transform: [{ scale: 1.15 }] }} />
+              ) : (
+                <EyeIcon size={20} color={placeHolderTextColorChange} style={{ marginLeft: 10, transform: [{ scale: 1.15 }] }} />
+              )}
+            </TouchableOpacity>
+          </View>
+          {showError ? (
+            <Text className="text-[#ff633e] text-[13px] pt-3">{errorMessage}</Text>
+          ) : null}
+        </View>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
+}
