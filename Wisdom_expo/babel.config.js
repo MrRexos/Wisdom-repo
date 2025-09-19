b5601bf41934e70d678daa9f46db588ba5c8a3e0
MrRexos <hernanz.reio@gmail.com>
2025-09-19 20:07:28 +0200

    ...

--- Wisdom_expo/babel.config.js
+++ Wisdom_expo/babel.config.js
@@ -6,7 +6,8 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
     plugins: [
-      'react-native-worklets-core/plugin',
+      'nativewind/babel',
+      'react-native-worklets/plugin',
       "react-native-reanimated/plugin",
     ],
   };
