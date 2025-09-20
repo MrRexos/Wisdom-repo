324612aa95e88e650dcc8f1668dc26345aaa10c8
MrRexos <hernanz.reio@gmail.com>
2025-09-20 20:58:18 +0200

    Failing 54 and styles

--- Wisdom_expo/babel.config.js
+++ Wisdom_expo/babel.config.js
@@ -1,12 +1,33 @@
+const nativewind = require("nativewind/babel");
+
 module.exports = function (api) {
   api.cache(true);
+
+  const nativewindConfig = nativewind();
+  const nativewindPlugins = Array.isArray(nativewindConfig?.plugins)
+    ? [...nativewindConfig.plugins]
+    : [];
+
+  const findPluginIndex = (plugins, name) =>
+    plugins.findIndex(
+      (plugin) =>
+        plugin === name ||
+        (Array.isArray(plugin) && plugin[0] === name)
+    );
+
+  const workletsIndex = findPluginIndex(
+    nativewindPlugins,
+    "react-native-worklets/plugin"
+  );
+
+  const plugins = [...nativewindPlugins];
+
+  if (workletsIndex === -1) {
+    plugins.push("react-native-worklets/plugin");
+  }
+
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      'react-native-worklets/plugin',
-    ],
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    plugins,
   };
 };
